c226596abf2ce68253386f237ea0b100c0a50666
luananicolau <luananicolauln@gmail.com>
2025-11-12 14:12:20 -0300

    style: HomeScreen background

--- app/src/main/java/com/example/mobiletest/ui/view/home/HomeScreen.kt
+++ app/src/main/java/com/example/mobiletest/ui/view/home/HomeScreen.kt
@@ -1,8 +1,33 @@
 package com.example.mobiletest.ui.view.home
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
+    val pinkBackground = Color(0xFFFF4081)
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(
+                    color = pinkBackground,
+                    shape = RoundedCornerShape(bottomStart = 50.dp, bottomEnd = 50.dp)
+                )
+                .align(Alignment.TopCenter)
+        )
+
+    }
 }
