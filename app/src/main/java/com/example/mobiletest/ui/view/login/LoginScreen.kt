19574a0d669d89548d7f282dd08e85e048ead46e
luananicolau <luananicolauln@gmail.com>
2025-11-12 11:09:54 -0300

    style:adjust login button

--- app/src/main/java/com/example/mobiletest/ui/view/login/LoginScreen.kt
+++ app/src/main/java/com/example/mobiletest/ui/view/login/LoginScreen.kt
@@ -3,6 +3,7 @@ package com.example.mobiletest.ui.view.login
 import android.R.color.white
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,6 +15,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun LoginScreen() {
 
+    val pinkBackground = Color(0xFFFF4081)
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -24,11 +27,18 @@ fun LoginScreen() {
         Spacer(Modifier.height(8.dp))
         OutlinedTextField(value = "", onValueChange = {}, label = { Text("Password") })
         Spacer(Modifier.height(16.dp))
+
         Button(
-            onClick = {},
-            modifier = Modifier.fillMaxWidth()
+            onClick = { },
+            modifier = Modifier
+                .width(350.dp)
+                .height(50.dp)
+                .align(Alignment.CenterHorizontally),
+            colors = ButtonDefaults.buttonColors(containerColor = pinkBackground),
+            shape = RoundedCornerShape(25.dp)
         ) {
             Text("LOGIN")
         }
+
     }
 }
