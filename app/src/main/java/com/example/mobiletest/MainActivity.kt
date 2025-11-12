bdceb4a988bfade6867b5ad233ef381e87182369
luananicolau <luananicolauln@gmail.com>
2025-11-12 14:09:36 -0300

    feat: create HomeScreen

--- app/src/main/java/com/example/mobiletest/MainActivity.kt
+++ app/src/main/java/com/example/mobiletest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.example.mobiletest.ui.view.home.HomeScreen
 import com.example.mobiletest.ui.view.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 LoginScreen()
+                HomeScreen()
             }
         }
     }
---
+++ app/src/main/java/com/example/mobiletest/ui/view/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.mobiletest.ui.view.home
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun HomeScreen() {
+
+}
