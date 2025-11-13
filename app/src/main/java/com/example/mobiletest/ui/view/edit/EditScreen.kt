d450b6e0bf92d5861a38f67b69b575f58ae16e04
luananicolau <luananicolauln@gmail.com>
2025-11-13 10:04:36 -0300

    style: update button color and size

--- app/src/main/java/com/example/mobiletest/ui/view/edit/EditScreen.kt
+++ app/src/main/java/com/example/mobiletest/ui/view/edit/EditScreen.kt
@@ -1,6 +1,7 @@
 package com.example.mobiletest.ui.view.edit
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -9,11 +10,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 @Composable
 fun EditScreen() {
+    val pink = Color(0xFFFF4081)
     var text by remember { mutableStateOf("") }
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.LightGray.copy(alpha = 0.3f)), // fundo leve só pra ver
+            .background(Color.LightGray.copy(alpha = 0.3f)), // fundo leve so pra ver
         contentAlignment = Alignment.BottomCenter
     ) {
         Column(
@@ -32,12 +34,27 @@ fun EditScreen() {
                 modifier = Modifier.fillMaxWidth()
             )
             Spacer(Modifier.height(16.dp))
-            Button(onClick = {  }, modifier = Modifier.fillMaxWidth()) {
-                Text("Confirmar")
+
+            Button(
+                onClick = { /* ação de confirmar */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = pink),
+                shape = RoundedCornerShape(25.dp)
+            ) {
+                Text("Confirm")
             }
-            Spacer(Modifier.height(8.dp))
-            Button(onClick = { }, modifier = Modifier.fillMaxWidth()) {
-                Text("Cancelar")
+
+            OutlinedButton(
+                onClick = { /* ação de cancelar */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                shape = RoundedCornerShape(25.dp),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = pink)
+            ) {
+                Text("Cancel")
             }
         }
     }
