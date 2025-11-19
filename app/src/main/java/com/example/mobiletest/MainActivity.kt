dfa97df65481d4e0af8ea49926a02cfbfdcf5053
luananicolau <luananicolauln@gmail.com>
2025-11-19 15:45:23 -0300

    feat: update edit logic

--- app/src/main/java/com/example/mobiletest/MainActivity.kt
+++ app/src/main/java/com/example/mobiletest/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mobiletest.ui.view.home.HomeScreen
 import com.example.mobiletest.ui.view.login.LoginScreen
 import com.example.mobiletest.ui.view.edit.EditScreen
+import com.example.mobiletest.ui.view.edit.EditViewModel
+import com.example.mobiletest.ui.view.home.HomeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,16 +38,20 @@ class MainActivity : ComponentActivity() {
                     }
 
                     composable("home") {
+                        val vm: HomeViewModel = viewModel()
+
                         HomeScreen(
+                            viewModel = vm,
                             onEditClick = {
                                 navController.navigate("edit")
                             }
                         )
                     }
-
                     composable("edit") {
+                        val vm: EditViewModel = viewModel()
 
                         EditScreen(
+                            viewModel = vm,
                             onSave = {
                                 navController.navigate("home")
                             },
@@ -51,10 +59,9 @@ class MainActivity : ComponentActivity() {
                                 navController.popBackStack()
                             }
                         )
-                    }
 
                 }
             }
         }
     }
-}
+}}
